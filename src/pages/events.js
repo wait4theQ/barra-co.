a960ce022fa8c57fbcdcf24cb101e73f1bc2d325
laura hobold marques <laurahobold@gmail.com>
2024-02-02 17:31:47 -0300

    added routing to events

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,18 +1,14 @@
+import React, {useState} from "react";
 
-import React from 'react';
-import NavbarMenu from './NavbarMenu';
-import Footer from './Footer';
 
-const Contato = () => {
-  return (
-    <div>
-      <NavbarMenu />
-      {/* Adicione outros componentes necessários para a página de Contato */}
-      <h1>Contato</h1>
-      {/* Conteúdo da página de Contato */}
-      <Footer />
-    </div>
-  );
-};
+const Events = () => {
 
-export default Contato;
+    return (
+        <div>
+            aaaaaaaaaaaaaaaaaaaaaaaa
+        </div>
+
+    );
+}
+
+export default Events;
